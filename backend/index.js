ec668d6439648f93360de775e604c2f3ac58d685
Jagan6923 <jeyaraman.saravanan23@gmail.com>
2025-04-07 23:42:34 +0530

    Refactor: Update CORS middleware configuration for improved security and functionality

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+        "https://cadd-tution-project-rnoe.vercel.app/"
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"]
+    })
+  );
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", coursesRoutes);
@@ -30,6 +39,10 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/leaves", leaveRoutes);
 app.use("/api/enrollments", StudentEnrollmentRoutes);
 
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the backend server!");
+});
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
